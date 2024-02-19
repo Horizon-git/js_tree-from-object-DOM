f0c46ff3a5279b40fe46613c567e50218a6030ea
Horizon-git <apbreloaded947@gmail.com>
2024-02-19 18:39:29 +0200

    make build

---
+++ index.0665c58a.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let t in n){let l=document.createElement("li");l.textContent=t,r.append(l),n[t]&&e(l,n[t])}t.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.0665c58a.js.map
